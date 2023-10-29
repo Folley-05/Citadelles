959aafe12ec9c644bc84efb218052c3ca6954c39
Y-DONALD <donaldyoungoua@gmail.com>
2023-10-29 21:05:13 +0100

    Add the Interaction.java class

---
+++ controleur/Interaction.java
@@ -0,0 +1,48 @@
+package controleur;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Interaction {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static int lireUnEntier() {
+        int i = 0;
+        boolean continu = true;
+        do {
+            try {
+                i = sc.nextInt();
+                continu = false;
+            } catch (InputMismatchException e) {
+                System.out.print("Veuillez rentrer un chiffre : ");
+                sc.next(); // passe l'entier pour �viter de boucler
+            }
+        } while(continu);
+        return i;
+    }
+
+    // renvoie un entier lu au clavier compris dans l'intervalle
+    //     [borneMin, borneMax[
+    public static int lireUnEntier(int borneMin, int borneMax) {
+        int i = 0;
+        // ...
+        return i;
+    }
+
+    // lit les r�ponses "oui", "non", "o" ou "n" et renvoie un bool�en
+    public static boolean lireOuiOuNon() {
+        boolean retour = true;
+        // ...
+        return retour;
+    }
+
+    // renvoie une cha�ne de caract�re lue au clavier:
+    public static String lireUneChaine() {
+        String retour = "";
+		//...
+        return retour;
+    }
+
+
+
+}
