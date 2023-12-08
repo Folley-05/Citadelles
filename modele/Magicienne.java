a88ed5e8a3a6a4964469e8d24e8cb7f9dfdda79f
Y-DONALD <donaldyoungoua@gmail.com>
2023-12-08 10:18:13 +0100

    Modify gitignore path

--- modele/Magicienne.java
+++ modele/Magicienne.java
@@ -1,10 +1,10 @@
+
 package modele;
 
-public class Magicienne extends Personnage{
-
-    public Magicienne() { super("Magicienne", 3, Caracteristiques.MAGICIENNE); }
-    @Override
-    public void utiliserPouvoir() {
-
+public class Magicienne extends Personnage {
+    public Magicienne() {
+        super("Magicienne", 3, Caracteristiques.MAGICIENNE);
     }
+    @Override
+    public void utiliserPouvoir() {}
 }
