a02893c470a6a3b173401ecb9bc837e372d507a8
Folley-05 <aristotepascaldjounda@gmail.com>
2023-12-12 14:39:14 +0100

    routine commit

--- modele/Assassin.java
+++ modele/Assassin.java
@@ -8,22 +8,24 @@ public class Assassin extends Personnage{
     public void utiliserPouvoir(){
         if(this.isValid()){
             int choix;
+            PlateauDeJeu plateau=this.getPlateau();
             // afficher les cibles
             System.out.println("Quel personnage voulez-vous assassiner ?");
-            for (int i = 0; i < this.getPlateau().getNombrePersonnages(); i++) {
-                if(this.getPlateau().getPersonnage(i)!= null) {
-                    System.out.println((i + 1) + " " + this.getPlateau().getPersonnage(i).getNom());
+            for (int i = 0; i < plateau.getNombrePersonnages(); i++) {
+                if(plateau.getPersonnage(i)!= null) {
+                    System.out.println((i + 1) + " " + plateau.getPersonnage(i).getNom());
                 }
             }
             //choix de la cible
             do {
                 System.out.print("Votre vhoix : ");
-                choix = Interaction.lireUnEntier(1, this.getPlateau().getNombrePersonnages()+1);
-                if(this.getPlateau().getPersonnage(choix -1).getNom().equals("Assassin"))
+                // choix = Interaction.lireUnEntier(1, plateau.getNombrePersonnages()+1);
+                choix=Interaction.automatedChoice(plateau.getNombreJoueurs(), true);
+                if(plateau.getPersonnage(choix -1).getNom().equals("Assassin"))
                     System.out.println("Vous ne pouvez pas vous assassiner !!");
-            } while (this.getPlateau().getPersonnage(choix -1).getNom().equals("Assassin"));
+            } while (plateau.getPersonnage(choix -1).getNom().equals("Assassin"));
             // assassinat de la cible
-            this.getPlateau().getPersonnage(choix - 1).setAssassine();
+            plateau.getPersonnage(choix - 1).setAssassine();
         }
     }
 }
